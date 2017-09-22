91a5dd5c25395bba858fcceea379c46baef6a695
jcaulfield2018 <jcaulfield2018@cchsraider.net>
2017-09-22 10:08:36 -0400

    initial commit

---
+++ HelloWorldSpaces/src/HelloWorldSpaces.java
@@ -0,0 +1,18 @@
+
+public class HelloWorldSpaces {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		System.out.println("World!");
+	}
+		public static void HelloWorldSpace(){
+		
+// output three blank lines
+			System.out.println();
+			System.out.println();
+			System.out.println();
+		
+	}
+
+}
